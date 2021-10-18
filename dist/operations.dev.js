bb5ad5c80c156dea46576410716a6b30e9fa1c85
sheetalkanago <42694813+sheetalkanago@users.noreply.github.com>
2021-10-18 16:06:03 +0530

    Created basic flow. Listerners, switch-case for buttons.

--- dist/operations.dev.js
+++ dist/operations.dev.js
@@ -1,12 +1,12 @@
-//variables
-let leftOperand = 0,
-  rightOperand = 0,
-  operator = "";
-let displayStr = "";
+"use strict";
 
-//get each DOM element in a const.
-const display = document.querySelector(".display");
-//  const buttonC = document.querySelector(".button--C");
+//variables
+var leftOperand = 0,
+    rightOperand = 0,
+    operator = "";
+var displayStr = ""; //get each DOM element in a const.
+
+var display = document.querySelector(".display"); //  const buttonC = document.querySelector(".button--C");
 // const buttonPlusMinus = document.querySelector(".button--plusMinus");
 // const buttonPercent = document.querySelector(".button--percent");
 // const buttonDivide = document.querySelector(".button--divide");
@@ -25,84 +25,89 @@ const display = document.querySelector(".display");
 // const buttonNum0 = document.querySelector(".button--num0");
 // const buttonDecimal = document.querySelector(".button--decimal");
 // const buttonEquals = document.querySelector(".button--equals");
-const buttons = document.querySelectorAll(".button");
-// console.log(buttons);
-buttons.forEach(button => button.addEventListener("click", () => handleClick(button.innerHTML)));
 
+var buttons = document.querySelectorAll(".button"); // console.log(buttons);
 
-//On number click - display it
+buttons.forEach(function (button) {
+  return button.addEventListener("click", function () {
+    return handleClick(button.innerHTML);
+  });
+}); //On number click - display it
 // buttonNum0.addEventListener("click",() => {  
 //   console.log("Button buttonNum0 clicked", buttonNum0.innerHTML);
 //   displayStr=displayStr+buttonNum0.innerHTML;
 //   display.value=displayStr;
 // });
 //-------------------------------------------
-
-
-
-
 //-------------------------------------------
 //MUST HAVE:
 //Take left and right operands and evaluate when = is clicked
-
 //handle decimals
-
 //GOOD TO HAVE
 //add comma at thousands place.
-
-
 //Keep typed string in a varable. When = is clicked, evaluate the string and display result
-
-
 //
+
 function handleClick(strKey) {
   console.log("Starting handleClick(" + strKey + ")");
+
   switch (strKey) {
-    case ("C"):
-      console.log("strKey => ", strKey);
-      return;
-    case ("±")://evaluate and then applu -/+
-      console.log("strKey => ", strKey);
-      return;
-    case ("%")://evaluate and then appl;y %
+    case "C":
       console.log("strKey => ", strKey);
       return;
 
-    case ("÷"):
-      console.log("strKey => ", strKey);
-      return;
-    case ("×"):
-      console.log("strKey => ", strKey);
-      return;
-    case ("−"):
-      console.log("strKey => ", strKey);
-      return;
-    case ("+"):
-      console.log("strKey => ", strKey);
-      return;
-    case ("="):
+    case "±":
+      //evaluate and then applu -/+
       console.log("strKey => ", strKey);
       return;
 
-    case ("0"):
+    case "%":
+      //evaluate and then appl;y %
       console.log("strKey => ", strKey);
       return;
-    case ("1"):
-    case ("2"):
-    case ("3"):
-    case ("4"):
-    case ("5"):
-    case ("6"):
-    case ("7"):
-    case ("8"):
-    case ("9"):
+
+    case "÷":
+      console.log("strKey => ", strKey);
+      return;
+
+    case "×":
+      console.log("strKey => ", strKey);
+      return;
+
+    case "−":
+      console.log("strKey => ", strKey);
+      return;
+
+    case "+":
+      console.log("strKey => ", strKey);
+      return;
+
+    case "=":
+      console.log("strKey => ", strKey);
+      return;
+
+    case "0":
+      console.log("strKey => ", strKey);
+      return;
+
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
       // console.log("strKey => ", strKey);
-      displayStr=displayStr+strKey;
-      display.value=displayStr;
+      displayStr = displayStr + strKey;
+      display.value = displayStr;
       return;
-    case ("."):
+
+    case ".":
       console.log("strKey => ", strKey);
       return;
+
     default:
       console.log("Default case");
       return;
