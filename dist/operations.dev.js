bfaa3287cee2774e03530c221dd922c79fe4634d
sheetalkanago <42694813+sheetalkanago@users.noreply.github.com>
2021-10-19 22:26:53 +0530

    decimals done, get set methods done

--- dist/operations.dev.js
+++ dist/operations.dev.js
@@ -25,39 +25,43 @@ buttons.forEach(function (button) {
 //MUST HAVE:
 //DONE//Take left and right operands and evaluate when = is clicked
 //DONE//When +/- or % is clicked, evaluate the expr and then apply these operators. 
-//Q//how do I write a generic split and evaluate instead of if/else for each operator?
-//chained operations - when second operator is clicked, evaluate prev expr and store result in left operand
-//handle decimals
+//Q//how do I write a generic split and evaluate instead of if/else for each operator? -- regex to be discssed next week. Ignore for now
+//DONE//chained operations - when second operator is clicked, evaluate prev expr and store result in left operand
+//handle decimals - trailing zeros. Multiple decimal places. //004.000500
+//e- for small decimals
 //GOOD TO HAVE
-//add comma at thousands place.
-//handle length of input > display - reduce font? till when??
-//
+// deal with 0.1+0.2 = 0.30000003
+//add comma at thousands place. USe set/get method for display and convert every time
+//handle length of input > display - reduce font? till when??. Handle this in set display function above.
+//long numbers using e and limit to 20 characters
 
 function handleClick(strKey) {
-  console.log("Starting handleClick(" + strKey + ")");
-
+  //console.log("Starting handleClick(" + strKey + ")");
   switch (strKey) {
     case "C":
       displayStr = "0";
-      display.value = displayStr;
+      setDisplay(displayStr); // display.value=displayStr;
+
       return;
 
     case "±":
       //evaluate and then applu -/+
-      console.log("strKey => ", strKey);
+      //console.log("strKey => ", strKey);
       result = evaluateExpr() * -1;
-      displayStr = result.toString();
-      console.log(displayStr);
-      display.value = displayStr;
+      displayStr = result.toString(); //console.log(displayStr);
+
+      setDisplay(displayStr); // display.value=displayStr; 
+
       return;
 
     case "%":
       //evaluate and then appl;y %
-      console.log("strKey => ", strKey);
+      //console.log("strKey => ", strKey);
       result = evaluateExpr() / 100;
-      displayStr = result.toString();
-      console.log(displayStr);
-      display.value = displayStr;
+      displayStr = result.toString(); //console.log(displayStr);
+
+      setDisplay(displayStr); // display.value=displayStpr; 
+
       return;
 
     case "÷":
@@ -75,24 +79,26 @@ function handleClick(strKey) {
       } else {
         //If an operator (assuming only 1) is already present in displayStr then...
         //Evaluate displayStr
-        result = evaluateExpr();
-        console.log("chained op - result=", result); //Put it in Left operand
+        result = evaluateExpr(); //console.log("chained op - result=", result);
+        //Put it in Left operand
 
-        leftOperand = result.toString();
-        console.log("chained op - leftOperand=", leftOperand); //display left operand and the new operator
+        leftOperand = result.toString(); //console.log("chained op - leftOperand=", leftOperand);
+        //display left operand and the new operator
 
         displayStr = leftOperand + strKey;
-        display.value = displayStr;
+        setDisplay(displayStr); // display.value=displayStr;        
       }
 
+      return;
     //end chained operations
 
     case "=":
       //evaluate expr and display
       result = evaluateExpr();
-      displayStr = result.toString();
-      console.log(displayStr);
-      display.value = displayStr;
+      displayStr = result.toString(); //console.log(displayStr);
+
+      setDisplay(displayStr); // display.value=displayStr;     
+
       return;
 
     case "0":
@@ -107,24 +113,57 @@ function handleClick(strKey) {
     case "9":
       // console.log("strKey => ", strKey);
       if (displayStr === "0") {
-        console.log("in displayStr=0");
+        //console.log("in displayStr=0");
         displayStr = strKey;
       } else {
         displayStr = displayStr + strKey;
       }
 
-      display.value = displayStr;
+      setDisplay(displayStr); // display.value=displayStr;
+
       return;
 
     case ".":
-      console.log("strKey => ", strKey);
+      //console.log("strKey => ", strKey);
+      //if operator exists, split string and chk if 2nd operand has decimal. If yes - ignore the decimal, if not, add it.
+      //If operator does not exist, check if decimal exists, if yes ignore this key else add it
+      if (binaryOperators.every(function (binaryOperator) {
+        return displayStr.indexOf(binaryOperator) < 0;
+      })) {
+        //operator does not exist
+        if (displayStr.indexOf(".") < 0) {
+          //decimal does not exist
+          displayStr = displayStr + strKey;
+          setDisplay(displayStr);
+          return;
+        } else {
+          //decimalalready exists - ignore.
+          return;
+        }
+      } else {
+        //operator exists - consider only right operand
+        var index = displayStr.indexOf("+") + displayStr.indexOf("−") + displayStr.indexOf("×") + displayStr.indexOf("÷") + 3;
+        rightOperand = displayStr.substring(index - 1);
+
+        if (rightOperand.indexOf(".") < 0) {
+          //decimal does not exist
+          displayStr = displayStr + strKey;
+          setDisplay(displayStr);
+          return;
+        } else {
+          //decimalalready exists - ignore.
+          return;
+        }
+      }
+
       return;
 
     default:
-      console.log("Default case");
+      //console.log("Default case");
       return;
   }
-}
+} //
+
 
 function evaluateExpr() {
   var result = 0; //how do I write a generic split and evaluate instead of if/else for each operator?
@@ -142,8 +181,8 @@ function evaluateExpr() {
 
     leftOperand = _displayStr$split2[0];
     rightOperand = _displayStr$split2[1];
-    console.log("evaluateExpr==>", leftOperand, operator, rightOperand);
-    result = parseInt(leftOperand) + parseInt(rightOperand);
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result = parseFloat(leftOperand) + parseFloat(rightOperand);
   } else if (displayStr.indexOf("−") > 0) {
     operator = "−";
 
@@ -153,8 +192,8 @@ function evaluateExpr() {
 
     leftOperand = _displayStr$split4[0];
     rightOperand = _displayStr$split4[1];
-    console.log("evaluateExpr==>", leftOperand, operator, rightOperand);
-    result = parseInt(leftOperand) - parseInt(rightOperand);
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result = parseFloat(leftOperand) - parseFloat(rightOperand);
   } else if (displayStr.indexOf("×") > 0) {
     operator = "×";
 
@@ -164,8 +203,8 @@ function evaluateExpr() {
 
     leftOperand = _displayStr$split6[0];
     rightOperand = _displayStr$split6[1];
-    console.log("evaluateExpr==>", leftOperand, operator, rightOperand);
-    result = parseInt(leftOperand) * parseInt(rightOperand);
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result = parseFloat(leftOperand) * parseFloat(rightOperand);
   } else if (displayStr.indexOf("÷") > 0) {
     operator = "÷";
 
@@ -175,12 +214,27 @@ function evaluateExpr() {
 
     leftOperand = _displayStr$split8[0];
     rightOperand = _displayStr$split8[1];
-    console.log("evaluateExpr==>", leftOperand, operator, rightOperand);
-    result = parseInt(leftOperand) / parseInt(rightOperand);
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result = parseFloat(leftOperand) / parseFloat(rightOperand);
   } else {
-    result = parseInt(displayStr);
+    result = parseFloat(displayStr);
   }
 
-  return result; //
-  //
+  return result;
+}
+
+function setDisplay(result) {
+  //add commas and set value
+  if (typeof result == 'number') {
+    display.value = result.toString();
+    return;
+  }
+
+  display.value = result;
+  return;
+}
+
+function getDisplay() {
+  //remove commas and return the value
+  return displayStr;
 }
--- operations.js
+++ operations.js
@@ -14,35 +14,41 @@ buttons.forEach(button => button.addEventListener("click", () => handleClick(but
 //MUST HAVE:
 //DONE//Take left and right operands and evaluate when = is clicked
 //DONE//When +/- or % is clicked, evaluate the expr and then apply these operators. 
-//Q//how do I write a generic split and evaluate instead of if/else for each operator?
-//DONE//chained operations - when second operator is clicked, evaluate prev expr and store result in left operand
-//handle decimals
+//Q//how do I write a generic split and evaluate instead of if/else for each operator? -- regex to be discssed next week. Ignore for now
 
+//DONE//chained operations - when second operator is clicked, evaluate prev expr and store result in left operand
+//handle decimals - trailing zeros. Multiple decimal places. //004.000500
+//e- for small decimals
 
 //GOOD TO HAVE
-//add comma at thousands place.
-//handle length of input > display - reduce font? till when??
-//
+// deal with 0.1+0.2 = 0.30000003
+//add comma at thousands place. USe set/get method for display and convert every time
+//handle length of input > display - reduce font? till when??. Handle this in set display function above.
+//long numbers using e and limit to 20 characters
+
 function handleClick(strKey) {
-  console.log("Starting handleClick(" + strKey + ")");
+  //console.log("Starting handleClick(" + strKey + ")");
   switch (strKey) {
     case ("C"):
       displayStr="0";
-      display.value=displayStr;
+      setDisplay(displayStr);
+      // display.value=displayStr;
       return;
     case ("±")://evaluate and then applu -/+
-    console.log("strKey => ", strKey);
+    //console.log("strKey => ", strKey);
       result=evaluateExpr()*-1; 
       displayStr=result.toString();
-      console.log(displayStr);
-      display.value=displayStr; 
+      //console.log(displayStr);
+      setDisplay(displayStr);
+      // display.value=displayStr; 
       return;
     case ("%")://evaluate and then appl;y %
-      console.log("strKey => ", strKey);
+      //console.log("strKey => ", strKey);
       result=evaluateExpr()/100; 
       displayStr=result.toString();
-      console.log(displayStr);
-      display.value=displayStr; 
+      //console.log(displayStr);
+      setDisplay(displayStr);
+      // display.value=displayStpr; 
       return;
     case ("÷"):
     case ("×"):
@@ -57,13 +63,14 @@ function handleClick(strKey) {
       }else{ //If an operator (assuming only 1) is already present in displayStr then...
         //Evaluate displayStr
         result=evaluateExpr();
-        console.log("chained op - result=", result);
+        //console.log("chained op - result=", result);
         //Put it in Left operand
         leftOperand=result.toString();
-        console.log("chained op - leftOperand=", leftOperand);
+        //console.log("chained op - leftOperand=", leftOperand);
         //display left operand and the new operator
         displayStr=leftOperand+strKey;
-        display.value=displayStr;        
+        setDisplay(displayStr);
+        // display.value=displayStr;        
       }      
       return;
       //end chained operations
@@ -71,8 +78,9 @@ function handleClick(strKey) {
       //evaluate expr and display
       result=evaluateExpr();  
       displayStr=result.toString();
-      console.log(displayStr);
-      display.value=displayStr;     
+      //console.log(displayStr);
+      setDisplay(displayStr);
+      // display.value=displayStr;     
       return;
 
     case ("0"):      
@@ -87,23 +95,46 @@ function handleClick(strKey) {
     case ("9"):
       // console.log("strKey => ", strKey);
       if (displayStr==="0"){
-        console.log("in displayStr=0");
+        //console.log("in displayStr=0");
         displayStr=strKey;
       }else{
         displayStr=displayStr+strKey;
       }
-      
-      display.value=displayStr;
+      setDisplay(displayStr);
+      // display.value=displayStr;
       return;
     case ("."):
-      console.log("strKey => ", strKey);
+      //console.log("strKey => ", strKey);
+      //if operator exists, split string and chk if 2nd operand has decimal. If yes - ignore the decimal, if not, add it.
+      //If operator does not exist, check if decimal exists, if yes ignore this key else add it
+      if(binaryOperators.every((binaryOperator)=> displayStr.indexOf(binaryOperator)<0)){//operator does not exist
+        if(displayStr.indexOf(".")<0){//decimal does not exist
+          displayStr=displayStr+strKey;
+          setDisplay(displayStr);
+          return; 
+        }else{//decimalalready exists - ignore.
+          return;
+        }        
+      }else{//operator exists - consider only right operand
+        const index=displayStr.indexOf("+")+displayStr.indexOf("−")+displayStr.indexOf("×")+displayStr.indexOf("÷")+3
+        rightOperand=displayStr.substring(index-1);
+        if(rightOperand.indexOf(".")<0){//decimal does not exist
+          displayStr=displayStr+strKey;
+          setDisplay(displayStr);
+          return; 
+        }else{//decimalalready exists - ignore.
+          return;
+        }         
+         
+      }
+
       return;
     default:
-      console.log("Default case");
+      //console.log("Default case");
       return;
   }
 }
-
+//
 function evaluateExpr(){
   let result=0;
   //how do I write a generic split and evaluate instead of if/else for each operator?
@@ -115,31 +146,41 @@ function evaluateExpr(){
   if(displayStr.indexOf("+") > 0){
     operator="+";
     [leftOperand,rightOperand]=displayStr.split(operator);
-    console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
-    result=parseInt(leftOperand)+parseInt(rightOperand);
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result=parseFloat(leftOperand)+parseFloat(rightOperand);
   }else if(displayStr.indexOf("−") > 0){
     operator="−";
     [leftOperand,rightOperand]=displayStr.split(operator);
-    console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
-    result=parseInt(leftOperand)-parseInt(rightOperand);
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result=parseFloat(leftOperand)-parseFloat(rightOperand);
   }else if(displayStr.indexOf("×") > 0){
     operator="×";
     [leftOperand,rightOperand]=displayStr.split(operator);
-    console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
-    result=parseInt(leftOperand)*parseInt(rightOperand);
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result=parseFloat(leftOperand)*parseFloat(rightOperand);
   }else if(displayStr.indexOf("÷") > 0){
     operator="÷";
     [leftOperand,rightOperand]=displayStr.split(operator);
-    console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
-    result=parseInt(leftOperand)/parseInt(rightOperand);     
+    //console.log("evaluateExpr==>", leftOperand,operator,rightOperand);
+    result=parseFloat(leftOperand)/parseFloat(rightOperand);     
   }else{
 
-    result=parseInt(displayStr);
+    result=parseFloat(displayStr);
   }
   return result;
-  //
-  //
+}
 
+function setDisplay(result){
+  //add commas and set value
+  if (typeof(result)=='number') {
+    display.value=result.toString(); 
+    return;
+  }
+  display.value=result
+  return;
+}
 
-
+function getDisplay() {
+ //remove commas and return the value
+ return displayStr; 
 }
